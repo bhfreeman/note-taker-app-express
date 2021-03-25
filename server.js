d41a55b94b69a9e4e4656fd39f5fa61519912a32
bhfreeman <brett.h.freeman@gmail.com>
2021-03-24 19:01:08 -0700

    finalized functionality of delete route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { v4: uuidv4 } = require('uuid');
 const path = require("path");
 const fs = require("fs");
 const { json } = require("express");
@@ -23,6 +24,8 @@ async function readNotes() {
 async function writeNotes(note) {
     const notes = await readNotes();
     const array = JSON.parse(notes);
+    const newId = uuidv4();
+    note.id = newId;
     array.push(note);
     const pushed = JSON.stringify(array);
     await fs.promises.writeFile('./db/db.json', pushed);
@@ -49,13 +52,22 @@ app.get("/api/notes", (req, res) => {
 });
 // Post route to save and create a new note
 app.post("/api/notes", (req, res) => {
-  let note = JSON.parse(req.body);
+    let note = req.body;   
   writeNotes(note);
   res.send(req.body)
 });
 // Delete route to delete a note
 app.delete("/api/notes/:id", (req, res) => {
   const { id } = req.params;
+  readNotes().then((data) => {
+      const array = JSON.parse(data);
+      const deleteIndex = array.findIndex(obj => obj["id"] === id);
+    //   console.log(array);
+      array.splice(deleteIndex,1);
+    //   console.log(deleteIndex);
+    //   console.log(array)
+      fs.writeFile('./db/db.json', JSON.stringify(array), (err) => err ? console.error(err): console.log("file written"));
+  }).then(res.redirect("back"))
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
